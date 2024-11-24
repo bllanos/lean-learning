bea43cdcfaae375be28fb56a74f465092906f658
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2024-11-24 09:12:33 -0500

    Functional Programming in Lean practice

--- functional_programming_in_lean/02_hello_world/024_worked_example_cat/Main.lean
+++ functional_programming_in_lean/02_hello_world/024_worked_example_cat/Main.lean
@@ -10,6 +10,12 @@ partial def dump (stream : IO.FS.Stream) : IO Unit := do
     dump stream
 
 def fileStream (filename : System.FilePath) : IO (Option IO.FS.Stream) := do
+  -- Without the file existence check, the program will terminate
+  -- with an uncaught exception:
+  -- """
+  -- uncaught exception: no such file or directory (error code: 2)
+  --   file: $FILENAME
+  -- """
   let fileExists ← filename.pathExists
   if not fileExists then
     let stderr ← IO.getStderr
@@ -19,9 +25,20 @@ def fileStream (filename : System.FilePath) : IO (Option IO.FS.Stream) := do
     let handle ← IO.FS.Handle.mk filename IO.FS.Mode.read
     pure (some (IO.FS.Stream.ofHandle handle))
 
+def help (outStream : IO.FS.Stream) : IO Unit := do
+  outStream.putStrLn "\
+    Usage: feline [FILE]...
+  Concatenate FILE(s) to standard output.
+  With no FILE, or when FILE is -, read standard input.\
+"
+
 def process (exitCode : UInt32) (args : List String) : IO UInt32 := do
   match args with
   | [] => pure exitCode
+  | "--help" :: args =>
+    let stdout ← IO.getStdout
+    help stdout
+    process exitCode args
   | "-" :: args =>
     let stdin ← IO.getStdin
     dump stdin
@@ -38,4 +55,6 @@ def process (exitCode : UInt32) (args : List String) : IO UInt32 := do
 def main (args : List String) : IO UInt32 :=
   match args with
   | [] => process 0 ["-"]
+  -- This would only support `--help` as the only argument
+  -- | ["--help"] => do
   | _ => process 0 args
