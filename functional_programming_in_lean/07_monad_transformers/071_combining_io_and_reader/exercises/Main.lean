f31dbe95087a03a84035e75be6854c25e1c7df02
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2025-01-04 18:43:34 -0500

    Functional Programming in Lean practice
    Version of doug that accepts a starting path as an argument

--- functional_programming_in_lean/07_monad_transformers/071_combining_io_and_reader/exercises/Main.lean
+++ functional_programming_in_lean/07_monad_transformers/071_combining_io_and_reader/exercises/Main.lean
@@ -1,10 +1,15 @@
 import doug
 
 def main (args : List String) : IO UInt32 := do
-  match Doug.configFromArgs args with
-  | Except.ok config =>
-    (Doug.dirTree (← IO.currentDir)).run config
-    pure 0
+  match Doug.parseArgs args with
+  | Except.ok parsedArgs =>
+    let startingDirectory := (Coe.coe <$> parsedArgs.startingDirectory).getD (← IO.currentDir)
+    if (← startingDirectory.pathExists) then
+      (Doug.dirTree startingDirectory).run parsedArgs.config
+      pure 0
+    else
+      IO.eprintln s!"The starting path argument \"{startingDirectory}\" does not exist."
+      pure 1
   | Except.error err =>
     IO.eprintln err
     IO.eprintln Doug.usage
--- functional_programming_in_lean/07_monad_transformers/071_combining_io_and_reader/exercises/doug/cli.lean
+++ functional_programming_in_lean/07_monad_transformers/071_combining_io_and_reader/exercises/doug/cli.lean
@@ -2,20 +2,41 @@ import doug.config
 
 namespace Doug
 
+structure ParsedArgs where
+  startingDirectory : Option String := none
+  config: Config := {}
+
+structure FoundArgs where
+  showHiddenFiles : Bool := false
+  useASCII : Bool := false
+  startingDirectory: Bool := false
+
 def usage : String :=
-  "Usage : doug [--ascii -a]
+  "Usage : doug [--ascii -a DIRECTORY]
 Options:
 \t--ascii\tUse ASCII characters to display the directory structure
-\t-a\tShow hidden files"
+\t-a\tShow hidden files
+\tDIRECTORY\tStart of the filesystem hierarchy to output"
 
-def matchArg (cfg : Config) : String → Except String Config
-    | "--ascii" => Except.ok { cfg with useASCII := true }
-    | "-a" => Except.ok { cfg with showHiddenFiles := true }
-    | arg => Except.error s!"unrecognized argument \"{arg}\""
+def matchArg : (FoundArgs × ParsedArgs) → String → Except String (FoundArgs × ParsedArgs)
+      | (foundArgs, parsedArgs), "--ascii" =>
+        if foundArgs.useASCII then
+          Except.error s!"duplicate argument \"--ascii\""
+        else
+          Except.ok ({foundArgs with useASCII := true }, { parsedArgs with config := { parsedArgs.config with useASCII := true } })
+      | (foundArgs, parsedArgs), "-a" =>
+        if foundArgs.showHiddenFiles then
+          Except.error s!"duplicate argument \"-a\""
+        else
+          Except.ok ({foundArgs with showHiddenFiles := true }, { parsedArgs with config := { parsedArgs.config with showHiddenFiles := true } })
+      | (foundArgs, parsedArgs), arg =>
+        if foundArgs.startingDirectory then
+          Except.error s!"expected at most one non-named (i.e. positional) argument, providing the starting directory"
+        else
+          Except.ok ({foundArgs with startingDirectory := true }, { parsedArgs with startingDirectory := arg })
 
--- This function does not detect duplicate valid arguments
--- and will stop validation at the first invalid argument
-def configFromArgs (args : List String) : Except String Config :=
-  args.foldlM matchArg {}
+-- This function will stop validation at the first invalid argument
+def parseArgs (args : List String) : Except String ParsedArgs :=
+  Prod.snd <$> (args.foldlM matchArg ({}, {}))
 
 end Doug
