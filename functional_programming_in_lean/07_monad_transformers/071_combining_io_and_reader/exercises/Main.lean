55f5f99c0c36eda7c030cbfc8d3834db22caee90
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2025-01-05 11:16:45 -0500

    Conditionally access current directory

--- functional_programming_in_lean/07_monad_transformers/071_combining_io_and_reader/exercises/Main.lean
+++ functional_programming_in_lean/07_monad_transformers/071_combining_io_and_reader/exercises/Main.lean
@@ -1,9 +1,14 @@
 import doug
 
+def getStartingDirectory (parsedArgs : Doug.ParsedArgs): IO System.FilePath :=
+  match parsedArgs.startingDirectory with
+  | some directory => pure directory
+  | none => IO.currentDir
+
 def main (args : List String) : IO UInt32 := do
   match Doug.parseArgs args with
   | Except.ok parsedArgs =>
-    let startingDirectory := (Coe.coe <$> parsedArgs.startingDirectory).getD (← IO.currentDir)
+    let startingDirectory ← getStartingDirectory parsedArgs
     if (← startingDirectory.pathExists) then
       (Doug.dirTree startingDirectory).run parsedArgs.config
       pure 0
