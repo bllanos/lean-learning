24dae71f45a97825af85cdf2b1c7c83ccc3bbd8f
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2025-06-01 11:14:32 -0400

    Lean FFI experiment
    - Demonstrate using Lean objects and arrays in the C program

--- ffi/c/main.c
+++ ffi/c/main.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <lean/lean.h>
 
-// TODO
-// extern uint64_t my_map(lean_obj_arg);
-
 extern void lean_initialize_runtime_module();
-extern void lean_initialize();
+// This would replace `lean_initialize_runtime_module()` if the code accesses the `Lean` package
+// extern void lean_initialize();
 extern void lean_io_mark_end_initialization();
-extern lean_object * initialize_MapArray(uint8_t builtin, lean_object *);
+
+extern lean_object* initialize_MapArray(uint8_t builtin, lean_object *w);
+extern lean_object* mk_map_options(uint32_t addend, uint32_t multiplicand);
+extern lean_object* map_options_to_string(lean_object*);
+extern lean_object* my_map(lean_object *options, lean_object *arr);
 
 int main() {
+  printf("Program start\n");
+
+  // Lean initialization
+  // -------------------
   lean_initialize_runtime_module();
-  lean_object * res;
+
+  // Lean module initialization
+  // --------------------------
+  lean_object* res;
   // Use same default as for Lean executables
   // See https://github.com/leanprover/lean4/blob/master/doc/dev/ffi.md#initialization
   uint8_t builtin = 1;
   res = initialize_MapArray(builtin, lean_io_mk_world());
   if (lean_io_result_is_ok(res)) {
+      // This seems to be an alternative to `lean_dec()` that can be used when
+      // the value is known not to be a scalar.
       lean_dec_ref(res);
   } else {
       lean_io_result_show_error(res);
@@ -25,10 +36,53 @@ int main() {
   }
   lean_io_mark_end_initialization();
 
-  // TODO actual program
+  // Program logic
+  // -------------
 
-  // lean_object * s = lean_mk_string("hello!");
-  // uint64_t l = my_length(s);
-  // printf("output: %ld\n", l);
-  printf("Hello world\n");
+  uint32_t addend = 2;
+  uint32_t multiplicand = 3;
+
+  lean_object* map_options = mk_map_options(addend, multiplicand);
+  // Avoid having `map_options_to_string()` destroy `map_options`
+  lean_inc(map_options);
+  lean_object* map_options_lean_str = map_options_to_string(map_options);
+  // Strings in Lean are null-terminated
+  char const * map_options_cstr = lean_string_cstr(map_options_lean_str);
+  printf("MapOptions instance: %s\n", map_options_cstr);
+  lean_dec_ref(map_options_lean_str);
+
+  // Scalar arrays don't seem to be well-supported yet,
+  // otherwise it would be nice to use one.
+  // lean_object* arr = lean_alloc_sarray(unsigned elem_size, size_t size, size_t capacity);
+
+  size_t arr_size = 6;
+  lean_object* arr = lean_alloc_array(arr_size, arr_size);
+  lean_object ** arr_data = lean_array_cptr(arr);
+
+  printf("Populating input array: [ ");
+  for(size_t i = 0; i < arr_size; ++i) {
+    assert(i * 5 < 256); // Ensure values fit in `uint8_t`
+    uint8_t value = i * 5;
+    // There are no functions for boxing `uint8_t` values specifically, so use
+    // `lean_box_uint32()`
+    *(arr_data + i) = lean_box_uint32(value);
+    printf("%d, ", value);
+  }
+  printf("]\n");
+
+  // Note: `my_map()` will call `lean_dec()` on all arguments.
+  lean_object * arr_out = my_map(map_options, arr);
+  arr_data = lean_array_cptr(arr_out);
+  arr_size = lean_array_size(arr_out);
+
+  printf("Output array: [ ");
+  for(size_t i = 0; i < arr_size; ++i) {
+    uint32_t value = lean_unbox_uint32(*(arr_data + i));
+    printf("%d, ", value);
+  }
+  printf("]\n");
+
+  lean_dec_ref(arr_out);
+
+  printf("Program end\n");
 }
