40a0047e284689da02f3dc022791df77c4b4c65e
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2025-07-01 11:47:38 -0400

    Re-run Rust builds when the Lean version changes

--- ffi/rust/lean_sys/build.rs
+++ ffi/rust/lean_sys/build.rs
@@ -1,8 +1,12 @@
 use std::env;
 use std::error::Error;
 use std::ffi::OsStr;
+use std::fmt;
+use std::fs::File;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::str::FromStr;
 
 use bindgen::builder;
 
@@ -11,21 +15,24 @@ const INLINE_FUNCTIONS_WRAPPER_NAME: &str = "lean_sys_inline_functions_wrapper";
 const LEAN_MODULE_DIRECTORY_NAME: &str = "map_array";
 
 struct LakeEnv {
+    elan_toolchain: LeanToolchainVersion,
     lean_githash: String,
     lean_sysroot: PathBuf,
 }
 
 impl LakeEnv {
+    const ELAN_TOOLCHAIN: &str = "ELAN_TOOLCHAIN";
     const LEAN_GITHASH: &str = "LEAN_GITHASH";
     const LEAN_SYSROOT: &str = "LEAN_SYSROOT";
 
     pub fn from_posix_env(env: &[u8]) -> Result<Self, Box<dyn Error>> {
-        env.split(|c| c.is_ascii_control()).try_fold(
-            Self {
-                lean_githash: String::new(),
-                lean_sysroot: PathBuf::new(),
-            },
-            |accumulator, slice| -> Result<Self, Box<dyn Error>> {
+        let tuple = env.split(|c| c.is_ascii_control()).try_fold(
+            (
+                None,
+                String::new(),
+                PathBuf::new(),
+            ),
+            |accumulator, slice| -> Result<(Option<LeanToolchainVersion>, String, PathBuf), Box<dyn Error>> {
                 let mut parts = slice.splitn(2, |c| *c == b'=');
                 let var = parts.next().ok_or_else(|| {
                     format!(
@@ -38,13 +45,30 @@ impl LakeEnv {
                     Some(value) => {
                         let var_str = str::from_utf8(var)?;
                         match var_str {
+                            Self::ELAN_TOOLCHAIN => {
+                                let value_str = str::from_utf8(value)?;
+                                if accumulator.0.is_none() {
+                                    Ok((
+                                        Some(FromStr::from_str(value_str)?),
+                                        accumulator.1,
+                                        accumulator.2,
+                                    ))
+                                } else {
+                                    Err(format!(
+                                        "duplicate {} variable in environment string",
+                                        Self::ELAN_TOOLCHAIN
+                                    )
+                                    .into())
+                                }
+                            }
                             Self::LEAN_GITHASH => {
                                 let value_str = str::from_utf8(value)?;
-                                if accumulator.lean_githash.is_empty() {
-                                    Ok(Self {
-                                        lean_githash: String::from(value_str),
-                                        ..accumulator
-                                    })
+                                if accumulator.1.is_empty() {
+                                    Ok((
+                                        accumulator.0,
+                                        String::from(value_str),
+                                        accumulator.2,
+                                    ))
                                 } else {
                                     Err(format!(
                                         "duplicate {} variable in environment string",
@@ -55,11 +79,12 @@ impl LakeEnv {
                             }
                             Self::LEAN_SYSROOT => {
                                 let value_str = str::from_utf8(value)?;
-                                if accumulator.lean_sysroot.as_os_str().is_empty() {
-                                    Ok(Self {
-                                        lean_sysroot: PathBuf::from(value_str),
-                                        ..accumulator
-                                    })
+                                if accumulator.2.as_os_str().is_empty() {
+                                    Ok((
+                                        accumulator.0,
+                                        accumulator.1,
+                                        PathBuf::from(value_str),
+                                    ))
                                 } else {
                                     Err(format!(
                                         "duplicate {} variable in environment string",
@@ -73,7 +98,35 @@ impl LakeEnv {
                     }
                 }
             },
-        )
+        )?;
+        match tuple.0 {
+            Some(elan_toolchain) => {
+                if tuple.1.is_empty() {
+                    Err(format!(
+                        "missing {} variable in environment string",
+                        Self::LEAN_GITHASH
+                    )
+                    .into())
+                } else if tuple.2.as_os_str().is_empty() {
+                    Err(format!(
+                        "missing {} variable in environment string",
+                        Self::LEAN_SYSROOT
+                    )
+                    .into())
+                } else {
+                    Ok(Self {
+                        elan_toolchain,
+                        lean_githash: tuple.1,
+                        lean_sysroot: tuple.2,
+                    })
+                }
+            }
+            None => Err(format!(
+                "missing {} variable in environment string",
+                Self::ELAN_TOOLCHAIN
+            )
+            .into()),
+        }
     }
 
     pub fn lean_sysroot_library_directory(&self) -> PathBuf {
@@ -90,6 +143,88 @@ impl LakeEnv {
 
     pub fn lean_header_path(&self) -> PathBuf {
         self.lean_include_directory().join("lean").join("lean.h")
+    }
+
+    pub fn elan_toolchain(&self) -> &LeanToolchainVersion {
+        &self.elan_toolchain
+    }
+
+    pub fn elan_toolchain_directory_path(&self) -> &Path {
+        self.lean_sysroot.parent().unwrap()
+    }
+
+    pub fn export_rustc_env(&self) {
+        println!(
+            "cargo::rustc-env={}={}",
+            Self::ELAN_TOOLCHAIN,
+            self.elan_toolchain
+        );
+        println!(
+            "cargo::rustc-env={}={}",
+            Self::LEAN_GITHASH,
+            self.lean_githash
+        );
+    }
+}
+
+#[derive(PartialEq, Eq)]
+enum LeanToolchainVersion {
+    Version(String),
+    Stable,
+}
+
+impl LeanToolchainVersion {
+    const STABLE_VERSION: &str = "stable";
+
+    pub fn toolchain_file_path_from_lake_package_path<P: AsRef<Path>>(
+        lake_package_path: P,
+    ) -> PathBuf {
+        lake_package_path.as_ref().join("lean-toolchain")
+    }
+
+    pub fn from_toolchain_file<P: AsRef<Path>>(
+        toolchain_file_path: P,
+    ) -> Result<Self, Box<dyn Error>> {
+        let mut toolchain_file = File::open(&toolchain_file_path).map_err(|err| {
+            format!(
+                "failed to open file \"{}\" to inspect Lean toolchain version: {}",
+                toolchain_file_path.as_ref().display(),
+                err
+            )
+        })?;
+
+        let mut version = String::new();
+        toolchain_file.read_to_string(&mut version).map_err(|err| {
+            format!(
+                "failed to read file \"{}\" to inspect Lean toolchain version: {}",
+                toolchain_file_path.as_ref().display(),
+                err
+            )
+        })?;
+
+        Self::from_str(&version)
+    }
+}
+
+impl FromStr for LeanToolchainVersion {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let trimmed_version = s.trim();
+        Ok(if trimmed_version == Self::STABLE_VERSION {
+            Self::Stable
+        } else {
+            Self::Version(trimmed_version.into())
+        })
+    }
+}
+
+impl fmt::Display for LeanToolchainVersion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Stable => f.write_str(Self::STABLE_VERSION),
+            Self::Version(str) => f.write_str(&str),
+        }
     }
 }
 
@@ -120,11 +255,53 @@ fn get_lake_environment<P: AsRef<Path>>(lake_package_path: P) -> Result<LakeEnv,
     }
 }
 
+fn rerun_build_if_lean_version_changes<P: AsRef<Path>>(
+    lake_package_path: P,
+    lake_env: &LakeEnv,
+) -> Result<(), Box<dyn Error>> {
+    let toolchain_file_path =
+        LeanToolchainVersion::toolchain_file_path_from_lake_package_path(lake_package_path);
+    println!("cargo:rerun-if-changed={}", toolchain_file_path.display());
+
+    let lean_toolchain_version = LeanToolchainVersion::from_toolchain_file(&toolchain_file_path)?;
+    match lean_toolchain_version {
+        LeanToolchainVersion::Stable => {
+            let elan_toolchain_directory = lake_env.elan_toolchain_directory_path();
+            println!(
+                "cargo:warning=specifying \"{}\" as the Lean toolchain version in \"{}\" will slow down Cargo builds because the Elan toolchains directory (\"{}\") must be monitored for changes to detect a Lean toolchain version change",
+                LeanToolchainVersion::STABLE_VERSION,
+                toolchain_file_path.display(),
+                elan_toolchain_directory.display()
+            );
+            println!(
+                "cargo:rerun-if-changed={}",
+                elan_toolchain_directory.display()
+            );
+            Ok(())
+        }
+        lean_toolchain_version => {
+            if &lean_toolchain_version == lake_env.elan_toolchain() {
+                Ok(())
+            } else {
+                Err(format!(
+                    "Lean toolchain from \"{}\", \"{}\", does not match the Lean toolchain from the Lake environment, \"{}\"",
+                    toolchain_file_path.display(),
+                    lean_toolchain_version,
+                    lake_env.elan_toolchain()
+                ).into())
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let lake_package_path = get_lake_package_path()?;
     let lake_environment = get_lake_environment(&lake_package_path)?;
     let lean_library_directory = lake_environment.lean_library_directory();
     let lean_sysroot_library_directory = lake_environment.lean_sysroot_library_directory();
+
+    lake_environment.export_rustc_env();
+    rerun_build_if_lean_version_changes(lake_package_path, &lake_environment)?;
 
     println!(
         "cargo:rustc-link-search={}",
--- ffi/rust/lean_sys/src/lib.rs
+++ ffi/rust/lean_sys/src/lib.rs
@@ -6,6 +6,9 @@
 #![allow(non_camel_case_types)]
 include!(env!("LEAN_RUST_BINDINGS"));
 
+pub const ELAN_TOOLCHAIN: &str = env!("ELAN_TOOLCHAIN");
+pub const LEAN_GITHASH: &str = env!("LEAN_GITHASH");
+
 unsafe extern "C" {
     pub unsafe fn lean_initialize_runtime_module();
     /// This would replace `lean_initialize_runtime_module()` if the code accesses the `Lean` package
