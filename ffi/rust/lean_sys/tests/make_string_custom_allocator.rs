1d27a3e859a24f71ea0b27d18c3edc796ec563cb
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2025-07-01 12:10:27 -0400

    Copy integration test in preparation for using a custom allocator

--- ffi/rust/lean_sys/tests/make_string_custom_allocator.rs
+++ ffi/rust/lean_sys/tests/make_string_custom_allocator.rs
@@ -1,7 +1,5 @@
-#![no_std]
-
-use core::ffi::CStr;
-use core::str::Utf8Error;
+use std::ffi::{CStr, CString};
+use std::str::{FromStr, Utf8Error};
 
 use lean_sys::{
     lean_dec_ref, lean_initialize_runtime_module, lean_io_mark_end_initialization, lean_mk_string,
@@ -9,30 +7,33 @@ use lean_sys::{
 };
 
 /// This test is a version of [`make_string_std()`](./make_string_std.rs) that
-/// does not rely on dynamic memory allocation in Rust.
+/// uses Lean's allocator (assumed to be mimalloc) for dynamic memory allocation
+/// in Rust.
 #[test]
-fn make_string_no_std() -> Result<(), Utf8Error> {
+fn make_string_custom_allocator() -> Result<(), Utf8Error> {
     unsafe {
         lean_initialize_runtime_module();
         lean_io_mark_end_initialization();
     }
 
+    let initial_string = String::from("Hello, world");
     let new_char = b'!';
-    let initial_cstring = b"Hello, world\0";
+    let initial_cstring = CString::from_str(&initial_string).unwrap();
     let initial_string_ptr = initial_cstring.as_ptr();
 
     let longer_lean_string: lean_obj_res;
     let final_cstring: &CStr;
 
     unsafe {
-        let lean_string = lean_mk_string(initial_string_ptr as *const i8);
+        let lean_string = lean_mk_string(initial_string_ptr);
         longer_lean_string = lean_string_push(lean_string, new_char as u32);
         let longer_lean_cstring = lean_string_cstr(longer_lean_string);
         final_cstring = CStr::from_ptr(longer_lean_cstring);
     }
 
     let final_string = final_cstring.to_str()?;
-    let expected_string = "Hello, world!";
+    let mut expected_string = initial_string.clone();
+    expected_string.push(new_char as char);
     assert_eq!(final_string, expected_string);
 
     unsafe {
--- ffi/rust/lean_sys/tests/make_string_std.rs
+++ ffi/rust/lean_sys/tests/make_string_std.rs
@@ -14,13 +14,22 @@ use lean_sys::{
 /// approach is used in [`make_string_no_std()`](./make_string_no_std.rs).
 ///
 /// Rather than use Lean's allocation functions for specific types of Lean
-/// objects, one could program using Rust objects but use Lean's allocator to
-/// allocate them. Using Lean's allocator with Rust's standard library objects
-/// looks fragile, however, because Rust code needs to be aware of Lean's
-/// build-time memory allocation configuration, specifically the `LEAN_MIMALLOC`
-/// and `LEAN_SMALL_ALLOCATOR` options. An example of using Lean's allocator as
-/// Rust's global allocator is shown at
+/// objects, one could program using Rust standard library objects but use
+/// Lean's allocator to allocate them. Using Lean's allocator with Rust's
+/// standard library objects is complicated because Rust code needs to be aware
+/// of Lean's build-time memory allocation configuration, specifically the
+/// `LEAN_MIMALLOC` and `LEAN_SMALL_ALLOCATOR` options. An example of using
+/// Lean's allocator as Rust's global allocator in this way is shown at
 /// <https://github.com/digama0/lean-sys/blob/dd7ff0cfa4a70ad8d1aecc7f8cb6ced776664c11/src/alloc.rs>.
+///
+/// We can probably assume that Lean is going to use mimalloc in preference to
+/// its special-purpose small object allocator for the foreseeable future, based
+/// on the discussion in the [mimalloc technical
+/// report](https://www.microsoft.com/en-us/research/publication/mimalloc-free-list-sharding-in-action),
+/// which says that mimalloc, "outperform[s] our own custom allocators for small
+/// objects in Lean". We demonstrate using a simple Rust global allocator that
+/// uses mimalloc in
+/// [`make_string_no_custom_allocator.rs`](./make_string_no_custom_allocator.rs).
 #[test]
 fn make_string_std() -> Result<(), Utf8Error> {
     unsafe {
