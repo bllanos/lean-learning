7ccb2cbdc6428537eb42da7189af3f264a2277bc
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2025-09-07 09:14:03 -0400

    Deduplicate global allocator definition

--- ffi/rust/lean_sys/tests/make_string_custom_allocator.rs
+++ ffi/rust/lean_sys/tests/make_string_custom_allocator.rs
@@ -1,47 +1,14 @@
-use std::alloc::{GlobalAlloc, Layout};
-use std::ffi::{CStr, CString, c_void};
+use std::ffi::{CStr, CString};
 use std::str::FromStr;
 
+use lean::MimallocAllocator;
 use lean_sys::{
     lean_dec_ref, lean_initialize_runtime_module, lean_io_mark_end_initialization, lean_mk_string,
-    lean_obj_res, lean_string_cstr, lean_string_push, mi_aligned_alloc, mi_free_size_aligned,
-    mi_realloc_aligned, mi_zalloc_aligned,
+    lean_obj_res, lean_string_cstr, lean_string_push,
 };
-
-struct MimallocAllocator {}
 
 #[global_allocator]
 static ALLOCATOR: MimallocAllocator = MimallocAllocator {};
-
-// Reference:
-// <https://github.com/purpleprotocol/mimalloc_rust/blob/000709797d05324e449739ab428180cbe1199712/src/lib.rs>
-unsafe impl GlobalAlloc for MimallocAllocator {
-    // Required trait methods
-
-    #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        unsafe { mi_aligned_alloc(layout.align(), layout.size()) as *mut u8 }
-    }
-
-    #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        unsafe {
-            mi_free_size_aligned(ptr as *mut c_void, layout.size(), layout.align());
-        }
-    }
-
-    // Provided trait methods that mimalloc also supports
-
-    #[inline]
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        unsafe { mi_zalloc_aligned(layout.size(), layout.align()) as *mut u8 }
-    }
-
-    #[inline]
-    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        unsafe { mi_realloc_aligned(ptr as *mut c_void, new_size, layout.align()) as *mut u8 }
-    }
-}
 
 /// This test is a version of [`make_string_std()`](./make_string_std.rs) that
 /// uses Lean's allocator (assumed to be mimalloc) for dynamic memory allocation
--- ffi/rust/map_array_sys/tests/map_array_custom_allocator.rs
+++ ffi/rust/map_array_sys/tests/map_array_custom_allocator.rs
@@ -1,54 +1,21 @@
-use std::alloc::{GlobalAlloc, Layout};
 use std::error::Error;
-use std::ffi::{CStr, c_void};
+use std::ffi::CStr;
 use std::slice;
 
+use lean::MimallocAllocator;
 use lean_sys::{
     lean_alloc_array, lean_array_cptr, lean_array_size, lean_box_uint32, lean_dec, lean_dec_ref,
     lean_inc, lean_initialize_runtime_module, lean_io_mark_end_initialization, lean_io_mk_world,
     lean_io_result_is_ok, lean_io_result_show_error, lean_object, lean_string_cstr,
-    lean_unbox_uint32, mi_aligned_alloc, mi_free_size_aligned, mi_realloc_aligned,
-    mi_zalloc_aligned,
+    lean_unbox_uint32,
 };
 use map_array_sys::{
     MapArray::Basic_c::{map_options_to_string, mk_map_options, my_map},
     MapArray_c::initialize_MapArray,
 };
 
-struct MimallocAllocator {}
-
 #[global_allocator]
 static ALLOCATOR: MimallocAllocator = MimallocAllocator {};
-
-// Reference:
-// <https://github.com/purpleprotocol/mimalloc_rust/blob/000709797d05324e449739ab428180cbe1199712/src/lib.rs>
-unsafe impl GlobalAlloc for MimallocAllocator {
-    // Required trait methods
-
-    #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        unsafe { mi_aligned_alloc(layout.align(), layout.size()) as *mut u8 }
-    }
-
-    #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        unsafe {
-            mi_free_size_aligned(ptr as *mut c_void, layout.size(), layout.align());
-        }
-    }
-
-    // Provided trait methods that mimalloc also supports
-
-    #[inline]
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        unsafe { mi_zalloc_aligned(layout.size(), layout.align()) as *mut u8 }
-    }
-
-    #[inline]
-    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        unsafe { mi_realloc_aligned(ptr as *mut c_void, new_size, layout.align()) as *mut u8 }
-    }
-}
 
 /// A test that uses Lean's allocator (assumed to be mimalloc) for dynamic
 /// memory allocation in Rust and exercises functions from the Lean `MapArray`
