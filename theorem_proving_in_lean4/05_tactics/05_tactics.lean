d3b030c6d5d434e5d29a33919040b5b272e9a55a
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2026-09-20 19:37:46 -0400

    Theorem Proving in Lean 4 practice

--- theorem_proving_in_lean4/05_tactics/05_tactics.lean
+++ theorem_proving_in_lean4/05_tactics/05_tactics.lean
@@ -35,3 +35,83 @@ theorem test5 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
     case left => exact hq
     case right => exact hp
   case left => exact hp
+
+theorem test6 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  · exact hp
+  · apply And.intro
+    · exact hq
+    · exact hp
+
+-- ## 5.2 Basic tactics
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  · intro h
+    apply Or.elim (And.right h)
+    · intro hq
+      apply Or.inl
+      apply And.intro
+      · exact And.left h
+      · exact hq
+    · intro hr
+      apply Or.inr
+      apply And.intro
+      · exact And.left h
+      · exact hr
+  · intro h
+    apply Or.elim h
+    · intro hpq
+      apply And.intro
+      · exact And.left hpq
+      · apply Or.inl
+        exact And.right hpq
+    · intro hpr
+      apply And.intro
+      · exact And.left hpr
+      · apply Or.inr
+        exact And.right hpr
+
+example (α : Type) : α → α := by
+  intro a
+  exact a
+
+example (α : Type) : ∀ x : α, x = x := by
+  intro x
+  exact Eq.refl x
+
+example : ∀ a b c : Nat, a = b → a = c → c = b := by
+  intro a b c h₁ h₂
+  exact Eq.trans (Eq.symm h₂) h₁
+
+example (p q : α → Prop) : (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x := by
+  intro ⟨w, hpw, hqw⟩
+  exact ⟨w, hqw, hpw⟩
+
+example (p q : α → Prop) : (∃ x, p x ∨ q x) → ∃ x, q x ∨ p x := by
+  intro
+  | ⟨w, Or.inl h⟩ => exact ⟨w, Or.inr h⟩
+  | ⟨w, Or.inr h⟩ => exact ⟨w, Or.inl h⟩
+
+section
+  variable (x y z w : Nat)
+
+  example (h₁ : x = y) (h₂ : y = z) (h₃ : z = w) : x = w := by
+    apply Eq.trans h₁
+    apply Eq.trans h₂
+    assumption -- h₃
+
+  example (h₁ : x = y) (h₂ : y = z) (h₃ : z = w) : x = w := by
+    apply Eq.trans
+    assumption
+    apply Eq.trans
+    assumption
+    assumption
+end
+
+example : ∀ a b c : Nat, a = b → a = c → c = b := by
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  assumption
+  assumption
